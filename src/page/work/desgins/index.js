89412068bd7251b1d669b115adcf2fafe89eef2f
Arthur Ou <270079245@qq.com>
2018-08-31 19:23:45 +0800

    en

--- src/page/work/desgins/index.js
+++
@@ -1,7 +0,0 @@
-import logo1 from './logo1'
-import logo2 from './logo2'
-import chicken from './chicken'
-const myDesgin = {
-  logo1, logo2, chicken
-}
-export default myDesgin
--- src/utils/isPhone.js
+++
@@ -1,2 +0,0 @@
-// 判断是否为移动端
-export default (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent))
